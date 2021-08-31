60b0e42b35f8428e63ce737d13a8c965f9ddd736
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2021-08-31 13:58:59 -0400

    Added babelrc file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
 
 const Home: NextPage = () => {
-  return <div>asd</div>;
+	return <Wrapper>asd</Wrapper>;
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+	color: red;
+`;
