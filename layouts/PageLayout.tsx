9ac7b7049a6bcc2147e202339b1751a3c08e0a00
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2021-09-02 13:40:31 -0400

    Refactored main page layout into its own component to use across all pages

---
+++ layouts/PageLayout.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+import { Sidebar } from '@/components/Sidebar/Sidebar';
+
+interface PageLayoutProps {
+	children: React.ReactNode;
+}
+
+const PageLayout = ({ children }: PageLayoutProps) => {
+	return (
+		<Container>
+			<Sidebar />
+			<Wrapper>{children}</Wrapper>
+		</Container>
+	);
+};
+
+export default PageLayout;
+
+const Container = styled.div`
+	display: flex;
+	flex-flow: column;
+	@media ${(p) => p.theme.QUERIES.laptopAndUp} {
+		flex-flow: row;
+	}
+`;
+
+const Wrapper = styled.main`
+	padding: 0 24px;
+	margin-top: 32px;
+	height: 100%;
+	width: 100%;
+
+	@media ${(p) => p.theme.QUERIES.tabletAndUp} {
+		margin: 0 auto;
+		margin-top: 56px;
+		padding: 0;
+
+		max-width: 672px;
+	}
+
+	@media ${(p) => p.theme.QUERIES.laptopAndUp} {
+		max-width: 730px;
+		margin-top: 72px;
+	}
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,32 @@
 import { NextPage } from 'next';
-import styled from 'styled-components';
 
 import { Header } from '@/components/Home';
 import { InvoiceCard } from '@/components/InvoiceCard/InvoiceCard';
 import { InvoiceListContainer } from '@/components/InvoiceListContainer/InvoiceListContainer';
-import { Sidebar } from '@/components/Sidebar/Sidebar';
+import PageLayout from '@/layouts/PageLayout';
 
 const Home: NextPage = () => {
 	return (
-		<Container>
-			<Sidebar />
-			<Wrapper>
-				<Header invoiceCount={0} />
-				<InvoiceListContainer>
-					<InvoiceCard
-						id="RT3080"
-						dueDate="Due 19 Aug 2021"
-						clientName="Jensen Huang"
-						total="£1,800.90"
-						status="Paid"
-					/>
-					<InvoiceCard
-						id="XM9141"
-						dueDate="Due  20 Sep 2021"
-						clientName="Alex Grim"
-						total="£ 556.00"
-						status="Pending"
-					/>
-				</InvoiceListContainer>
-			</Wrapper>
-		</Container>
+		<PageLayout>
+			<Header invoiceCount={0} />
+			<InvoiceListContainer>
+				<InvoiceCard
+					id="RT3080"
+					dueDate="Due 19 Aug 2021"
+					clientName="Jensen Huang"
+					total="£1,800.90"
+					status="Paid"
+				/>
+				<InvoiceCard
+					id="XM9141"
+					dueDate="Due  20 Sep 2021"
+					clientName="Alex Grim"
+					total="£ 556.00"
+					status="Pending"
+				/>
+			</InvoiceListContainer>
+		</PageLayout>
 	);
 };
 
 export default Home;
-
-const Container = styled.div`
-	display: flex;
-	flex-flow: column;
-	@media ${(p) => p.theme.QUERIES.laptopAndUp} {
-		flex-flow: row;
-	}
-`;
-
-const Wrapper = styled.main`
-	padding: 0 24px;
-	margin-top: 32px;
-	height: 100%;
-	width: 100%;
-
-	@media ${(p) => p.theme.QUERIES.tabletAndUp} {
-		margin: 0 auto;
-		margin-top: 56px;
-		padding: 0;
-
-		max-width: 672px;
-	}
-
-	@media ${(p) => p.theme.QUERIES.laptopAndUp} {
-		max-width: 730px;
-		margin-top: 72px;
-	}
-`;
